ff3aeca8b2eb94049594e95e3f564cac46afa8e9
Marcos <mrcs.stark@gmail.com>
2020-07-08 00:27:51 -0300

    chore: Implementa modelos basicos da aplicacao

---
+++ src/main/java/dev/marcosouza/forum/model/Topic.java
@@ -0,0 +1,113 @@
+package dev.marcosouza.forum.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Topic {
+
+	private Long id;
+	private String title;
+	private String message;
+	private LocalDateTime createDate = LocalDateTime.now();
+	private TopicStatus status = TopicStatus.NOT_ANSWERED;
+	private User author;
+	private Course course;
+	private List<Answer> answers = new ArrayList<>();
+
+	public Topic(String title, String message, Course course) {
+		this.title = title;
+		this.message = message;
+		this.course = course;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Topic other = (Topic) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+
+	public TopicStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(TopicStatus status) {
+		this.status = status;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}
+
+}
---
+++ src/main/java/dev/marcosouza/forum/model/TopicStatus.java
@@ -0,0 +1,10 @@
+package dev.marcosouza.forum.model;
+
+public enum TopicStatus {
+	
+	NOT_ANSWERED,
+	NOT_SOLVED,
+	SOLVED,
+	CLOSED;
+
+}
